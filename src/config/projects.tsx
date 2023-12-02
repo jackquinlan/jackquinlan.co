e88f8d6fd6663dde96fe1f9ab08398df449f0409
jackquinlan <jquinlan@maxpayload.com>
2023-12-01 22:57:38 -0500

    added workspace

--- src/config/projects.tsx
+++ src/config/projects.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 
+import { CrumpledPaperIcon } from "@radix-ui/react-icons";
 import { Shapes } from "lucide-react";
 
 export type Project = {
@@ -18,4 +19,11 @@ export const PROJECTS: Project[] = [
         github: "https://github.com/jackquinlan/blueprint",
         icon: <Shapes className="h-4 w-4" />,
     },
+    {
+        name: "Workspace",
+        desc: "A project management tool that helps you organize your work, collaborate with your team, and get more done.",
+        website: "https://work.jackquinlan.co/",
+        github: "https://github.com/jackquinlan/workspace",
+        icon: <CrumpledPaperIcon className="h-4 w-4" />,
+    }
 ];
